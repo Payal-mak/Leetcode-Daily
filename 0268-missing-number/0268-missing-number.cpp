502eee2a5a22dc84d78144d8fcc23db6c7918f92
Payal Makwana <iampayal018@gmail.com>
2026-03-14 18:25:27 +0530

    Time: 0 ms (100%), Space: 21.5 MB (99.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int x1 = 0, x2 = 0;
+        int x1 = nums.size();
         //x1 = XOR of all numbers from 1 to N
         //x2 = XOR of all numbers present in nums
-        for(int i = 1; i <= nums.size(); i++){
-            x1 ^= i;
+        for(int i = 0; i < nums.size(); i++){
+            x1 ^= i ^ nums[i];
         }
-        for(int num : nums){
-            x2 ^= num;
-        }
-        return x1 ^ x2;
+        return x1;
     }
 };
