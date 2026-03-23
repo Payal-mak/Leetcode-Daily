fdc52914849361c5acaeda296575c70b4f047c2b
Payal Makwana <iampayal018@gmail.com>
2026-03-23 20:23:52 +0530

    Time: 0 ms (100%), Space: 22.6 MB (79.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,20 +4,11 @@ public:
         //two pointer approach
         int i = 0;
         for(int j = 1; j < nums.size(); j++){
-            //we check if arr[j] == arr[i]
-            //if yes, then continue
-            //if no, then do i++;
-            if(nums[j] == nums[i]){
-                continue;
+            if(nums[j] != nums[i]){
+                nums[i+1] = nums[j];
+                i++;
             }
-            i++;
-            nums[i] = nums[j];
         }
-        //now i have to return count and also array till i
-        int count = 0;
-        for(int j = 0; j <= i; j++){
-            count++;
-        }
-        return count;
+        return i+1;
     }
 };
