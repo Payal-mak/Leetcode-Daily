fdbde64dabbe889cd5a4b80f04e0df14a7d79024
Payal Makwana <iampayal018@gmail.com>
2026-03-23 22:50:39 +0530

    Time: 4 ms (4.23%), Space: 29.6 MB (42.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        //brute force approach
+        //optimal approach
+        //ex: nums[] = [1,2,3,4,5], k = 2
         int n = nums.size();
         k = k % n;
+        //first reverse the whole array
+        //nums[] = [5,4,3,2,1]
         reverse(nums.begin(), nums.end());
+        //reverse the first k elements
+        //nums[4,5,3,2,1]
         reverse(nums.begin(), nums.begin()+k);
+        //reverse the remaining n-k elements
+        //nums[] = [4,5,1,2,3]
         reverse(nums.begin()+k, nums.end());
     }
 };
