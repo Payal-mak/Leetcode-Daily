39d27331bb425bb81a44e05a4fd95b2c9da8a789
Payal Makwana <iampayal018@gmail.com>
2026-03-26 16:22:58 +0530

    Time: 11 ms (13.66%), Space: 23.8 MB (12.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //better approach
+        //using maps
+        int ans = 0;
+        unordered_map<int, int> mpp;
+        for(int i = 0; i < nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it : mpp){
+            if(it.second == 1){
+                ans = it.first;
+            }
+        }
+        return ans;
+    }
+};
