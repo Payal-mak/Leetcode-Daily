dfcb08316af7256d2307b04446eb0e17c138508f
Payal Makwana <iampayal018@gmail.com>
2026-03-27 00:54:16 +0530

    Time: 0 ms (100%), Space: 28.1 MB (63.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //better approach
-        //using hashmaps
-        int n = nums.size();
-        unordered_map<int, int> mpp;
+        //optimal approach
+        //moore voting algorithm
+        int count = 0;
+        int candidate = 0;
 
-        for(int i = 0; i < n; i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second > n/2){
-                return it.first;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num; //if it is a new element, whose count is 0, candidate = num
+            }
+            //if it is the same element, then simply do count++
+            if(num == candidate){
+                count++;
+            }
+            //if it is different element, then decrement count
+            else{
+                count--;
             }
         }
-        return 0;
+        //If the majority element has more than n/2 occurrences:
+        //The algorithm will ensure that the count remains positive for the majority element throughout the traversal, guaranteeing that it will be selected as the final candidate.
+        return candidate;
     }
 };
