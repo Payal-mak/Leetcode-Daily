5a4a3e11c5498fed444f5db09d20f4505370028e
Payal Makwana <iampayal018@gmail.com>
2026-05-15 00:41:25 +0530

    Time: 3 ms (39.77%), Space: 8.5 MB (31.08%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result = "";
-        int i = 0;
-        while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) {
-                result += word1[i];
+        // two pointers
+        int word1_size = word1.size();
+        int word2_size = word2.size();
+        int max_size = max(word1_size, word2_size);
+
+        string ans = "";
+
+        for(int i = 0; i < max_size; i++) {
+
+            if(i < word1_size) {
+                ans.push_back(word1[i]);
             }
-            if (i < word2.length()) {
-                result += word2[i];
+
+            if(i < word2_size) {
+                ans.push_back(word2[i]);
             }
-            i++;
         }
-        return result;
+
+        return ans;
     }
 };
