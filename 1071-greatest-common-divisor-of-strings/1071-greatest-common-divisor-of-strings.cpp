dc00f76b92895c58a888ef140702a65670485912
Payal Makwana <iampayal018@gmail.com>
2026-05-15 01:19:26 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.35%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    int gcd(int a, int b){
+        if(a == 0) return b;
+        return gcd(b%a,  a);
+    }
     string gcdOfStrings(string str1, string str2) {
         return (str1 + str2 == str2 + str1)?
         str1.substr(0,gcd(size(str1), size(str2))):"";
