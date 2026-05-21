58d8fdfffe3b625e0d8a37d6e265b6025baebb56
Payal Makwana <iampayal018@gmail.com>
2026-05-22 00:22:06 +0530

    Time: 0 ms (100%), Space: 10.3 MB (15.07%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string res = "";
+        for(int i = 0; i < s.size(); i++){
+            if(isalnum(s[i])){
+                res += tolower(s[i]);
+            }
+        }
+        int i = 0;
+        int j = res.size()-1;
+        while(i < j){
+            if(res[i] != res[j]){
+                return false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
