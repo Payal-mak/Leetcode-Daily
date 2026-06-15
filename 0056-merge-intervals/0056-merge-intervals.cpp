210d4e9797e7f55c1e89f1fea0b661f51c379d0c
Payal Makwana <iampayal018@gmail.com>
2026-06-15 14:43:43 +0530

    Time: 8 ms (24.72%), Space: 23.8 MB (81.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,27 +5,17 @@ public:
         sort(intervals.begin(), intervals.end());
 
         vector<vector<int>> ans;
-        //brute-force approach
+        //optimized approach
         for(int i = 0; i < n; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!ans.empty() && end <= ans.back()[1]){
-                //then that element is already lying in the interval
-                continue;
+            //if the start element of interval is not a part of previous interval
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                //then create a new interval
+                ans.push_back(intervals[i]);
             }
-            for(int j = i+1; j < n; j++){
-                //if the starting is lesser than ending of previous interval
-                //then it is overlapping
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                    //if not overlapping
-                    break;
-                }
+            else{
+                //if second element of interval lies in the previous interval
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
     }
