75db41a104edaab52481836b5d62250ba883b5cc
Payal Makwana <iampayal018@gmail.com>
2026-06-25 08:37:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,53 @@
 class Solution {
 public:
+    int rec(int n){
+        //base cases
+        if(n == 1) return 1;
+        if(n == 2) return 2;
 
-    int climbStairs(int n, vector<int>& memo){
-        //base case
-        if(n<=2) return n;
-        //rf
-        if(memo[n] != -1){
-            return memo[n];
-        }
-        return memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+        //rec function
+        return rec(n-1) + rec(n-2);
     }
+
+    int memoiz(vector<int>& memo, int i){
+        //check if a value is already computed before
+        if(memo[i] != -1){
+            return memo[i];
+        }
+        //store the computed result in the memo
+        return (memo[i] = memoiz(memo, i-1) + memoiz(memo, i-2));
+    }
+    int memoization(int n){
+        //base cases
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        vector<int> memo(n,-1);
+        //identify repeated steps
+        memo[0] = 1;
+        memo[1] = 2;
+        return memoiz(memo, n-1);
+    }
+
+    int solve_dp(vector<int>& dp, int n){
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+
+    int tabulation(int n){
+        //base cases
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 2;
+        return solve_dp(dp, n);
+        
+    }
+
     int climbStairs(int n) {
-        vector<int> memo(n+1, -1);
-        return climbStairs(n, memo);
+        return tabulation(n);
     }
 };
