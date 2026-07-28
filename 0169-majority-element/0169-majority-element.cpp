9e7dce84b6e1cb4648d9049e85cd76a1112fc0aa
Payal Makwana <iampayal018@gmail.com>
2026-07-29 00:36:04 +0530

    Time: 0 ms (100%), Space: 28.1 MB (92.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,16 +4,14 @@ public:
         int n = nums.size();
         int candidate = -1;
         int votes = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i<n ;i++){
             if(votes == 0){
                 candidate = nums[i];
             }
             if(nums[i] == candidate){
                 votes++;
             }
-            else{
-                votes--;
-            }
+            else votes--;
         }
         return candidate;
     }
