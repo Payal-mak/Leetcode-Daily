06dceac3397ceebacd7dfd308d9750d4b8a7617e
Payal Makwana <iampayal018@gmail.com>
2026-08-01 23:21:19 +0530

    Time: 0 ms (100%), Space: 53.8 MB (61.67%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -3,16 +3,25 @@
  * @return {number}
  */
 var sumOddLengthSubarrays = function(arr) {
-    let total_sum = 0;
     const n = arr.length;
-    for(let i = 0; i<n; i++){
-        let sum = 0;
-        for(let j = i; j<n; j++){
-            sum += arr[j];
-            if((j-i+1)%2 != 0){
-                total_sum += sum;
+    const prefix = [];
+    prefix[0] = arr[0];
+    for(let i = 1; i<n; i++){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+    // for(let i = 0; i<n; i++){
+    //     console.log(prefix[i]);
+    // }
+    let ans = 0;
+    for(let st = 0; st<n; st++){
+        for(let end = st; end<n; end+=2){
+            if(st === 0){
+                ans += prefix[end];
+            }
+            else{
+                ans += prefix[end] - prefix[st-1];
             }
         }
     }
-    return total_sum;
+    return ans;
 };
