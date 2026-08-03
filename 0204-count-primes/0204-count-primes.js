186289b2479f3d2f295476cc4b5949924eedcd6c
Payal Makwana <iampayal018@gmail.com>
2026-08-03 19:23:24 +0530

    Time: 259 ms (29.6%), Space: 119.5 MB (14.67%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if(n<2) return 0;
+    const sieve = new Array(n).fill(true);
+    sieve[0] = sieve[1] = false;
+    for(let i = 2; i*i<n; i++){
+        if(sieve[i]){
+            for(let j = i*i; j<n; j+=i){
+                sieve[j] = false;
+            }
+        }
+    }
+    return sieve.filter(Boolean).length;
+};
